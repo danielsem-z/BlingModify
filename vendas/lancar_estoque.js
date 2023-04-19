0abd31a12575801761b9378b76964371705501df
Daniel de Oliveira Silva <66263702+danielsem-z@users.noreply.github.com>
2023-04-19 00:37:27 -0300

    Update lancar_estoque.js

--- vendas/lancar_estoque.js
+++ vendas/lancar_estoque.js
@@ -11,22 +11,22 @@
 // ==/UserScript==
 
 const intervalo = setInterval(()=>{
-	const container = document.querySelector('[class="main-actions"]');
+    const container = document.querySelector('[class="main-actions"]');
     if(container){
-		const button = container.insertBefore(document.createElement("button"), container.querySelector("button:first-child"));
-		button.textContent = "Lançar estoque de todos os itens";
-		button.style.color = "black";
-		button.style.backgroundColor = "lightblue";
-		button.style.fontWeight = "bold";
-		button.style.textAlign = "center";
-		button.addEventListener("click", ()=>{
-			vendas.venda.displayWait();
-			fetch("http://127.0.0.1:5000/lancar_estoque", {"mode":"no-cors"}).then(()=>{
-				vendas.venda.closeWait();
-				document.querySelector('[class="refresh-button"]').click();
-			});
-		});
-		
+        const button = container.insertBefore(document.createElement("button"), container.querySelector("button:first-child"));
+        button.textContent = "Lançar estoque de todos os itens";
+        button.style.color = "black";
+        button.style.backgroundColor = "lightblue";
+        button.style.fontWeight = "bold";
+        button.style.textAlign = "center";
+        button.addEventListener("click", ()=>{
+            window.vendas.venda.displayWait();
+            fetch("http://127.0.0.1:5000/lancar_estoque", {"mode":"no-cors"}).then(()=>{
+                window.vendas.venda.closeWait();
+                document.querySelector('[class="refresh-button"]').click();
+            });
+        });
+
         clearInterval(intervalo);
     }
 }, 500);
