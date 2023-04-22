6e8443cfe48237d6a03de2769b10c05fcd60dd10
Daniel de Oliveira Silva <66263702+danielsem-z@users.noreply.github.com>
2023-04-22 12:45:49 -0300

    Update lancar_estoque.js
    Tava causando erro no lugar que eu estava incluindo

--- vendas/lancar_estoque.js
+++ vendas/lancar_estoque.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Lançar Estoque Geral
 // @namespace    https://github.com/danielsem-z
-// @version      0.1
+// @version      0.2
 // @description  script para lançar estoque de todas as vendas realizadas no PDV
 // @author       danielsem.z
 // @updateURL    https://raw.githubusercontent.com/danielsem-z/BlingModify/main/vendas/lancar_estoque.js
@@ -11,14 +11,18 @@
 // ==/UserScript==
 
 const intervalo = setInterval(()=>{
-    const container = document.querySelector('[class="main-actions"]');
+    const container = document.querySelector('[class="new-side-bar side-bar-overflow"]');
     if(container){
-        const button = container.insertBefore(document.createElement("button"), container.querySelector("button:first-child"));
+        const button = container.insertBefore(document.createElement("button"), container.querySelector("div:first-child"));
         button.textContent = "Lançar estoque de todos os itens";
-        button.style.color = "black";
         button.style.backgroundColor = "lightblue";
         button.style.fontWeight = "bold";
-        button.style.textAlign = "center";
+        button.style.justifyContent = "center";
+        button.style.alignItems = "center";
+        button.style.display = "flex";
+        button.style.width = "100%";
+        button.style.height = "40px";
+
         button.addEventListener("click", ()=>{
             window.vendas.venda.displayWait();
             fetch("http://127.0.0.1:5000/lancar_estoque", {"mode":"no-cors"}).then(()=>{
